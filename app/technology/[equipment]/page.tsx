6787c479143051a30a184a31d24308ff421e885d
VillageR88 <villager88pl@gmail.com>
2024-07-12 12:43:29 +0200

    mobile vehicles initial
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/technology/[equipment]/page.tsx
+++ app/technology/[equipment]/page.tsx
@@ -35,7 +35,7 @@ export default function Crew({ params }: { params: { equipment: string } }) {
 
   return (
     <div className="flex size-full flex-col-reverse items-center justify-between gap-[32px] pb-24 xl:max-h-[734px] xl:flex-row xl:gap-0 xl:pb-0">
-      <div className="flex size-full max-h-[304px] flex-col items-center justify-between gap-[40px] xl:max-w-[635px] xl:flex-row xl:gap-0">
+      <div className="flex size-full flex-col items-center justify-between gap-[40px] md:max-h-[304px] xl:max-w-[635px] xl:flex-row xl:gap-0">
         <ul className="flex size-fit gap-[32px] xl:flex-col">
           {Object.entries(RoutesTechnology).map((item, index) => (
             <li className="group/li flex flex-col justify-between" key={item[0]}>
@@ -43,7 +43,7 @@ export default function Crew({ params }: { params: { equipment: string } }) {
                 <button
                   title={item[0].replace(/-/g, ' ').toUpperCase()}
                   type="button"
-                  className={`size-[56px] rounded-full border font-bellefair text-[24px] transition xl:size-[80px] xl:text-[32px] ${option === item[0] ? 'border-white bg-white text-veryDarkNavy' : 'border-white/25 bg-transparent text-white hover:border-white'}`}
+                  className={`size-[40px] rounded-full border font-bellefair text-[18px] transition md:size-[56px] md:text-[24px] xl:size-[80px] xl:text-[32px] ${option === item[0] ? 'border-white bg-white text-veryDarkNavy' : 'border-white/25 bg-transparent text-white hover:border-white'}`}
                 >
                   {index + 1}
                 </button>
@@ -51,14 +51,14 @@ export default function Crew({ params }: { params: { equipment: string } }) {
             </li>
           ))}
         </ul>
-        <div className="flex size-full max-h-[301px] max-w-[491px] flex-col items-center text-center xl:items-start xl:text-start">
-          <span className="font-bellefair text-[24px] leading-tight text-white/[50.42%] xl:text-[32px]">
+        <div className="flex size-full max-w-[491px] flex-col items-center px-[24px] text-center md:max-h-[301px] md:px-0 xl:items-start xl:text-start">
+          <span className="font-bellefair text-[18px] leading-tight text-white/[50.42%] md:text-[24px] xl:text-[32px]">
             {titleTerminology}
           </span>
-          <h1 className="mt-[16px] font-bellefair text-[40px] leading-tight text-white xl:text-[56px]">
+          <h1 className="mt-[16px] font-bellefair text-[24px] leading-tight text-white md:text-[40px] xl:text-[56px]">
             {option.replace(/-/g, ' ').toUpperCase()}
           </h1>
-          <p className="mt-[24px] font-barlow text-[16px] leading-[180%] text-lightBlue xl:text-[18px]">
+          <p className="mt-[24px] font-barlow text-[15px] leading-[180%] text-lightBlue md:text-[16px] xl:text-[18px]">
             {equipmentItems[option].description}
           </p>
         </div>
@@ -73,7 +73,7 @@ export default function Crew({ params }: { params: { equipment: string } }) {
             alt={option}
           />
           <Image
-            className={`relative -z-10 block h-[357px] w-fit object-cover xl:hidden xl:h-[322px] ${option === 'spaceport' ? 'object-[100%_10%]' : ''}`}
+            className={`relative -z-10 block h-[258px] w-fit object-cover md:h-[357px] xl:hidden xl:h-[322px] ${option === 'spaceport' ? 'object-[100%_10%]' : ''}`}
             width={608}
             height={600}
             src={equipmentItems[option].srcMobileTablet}
